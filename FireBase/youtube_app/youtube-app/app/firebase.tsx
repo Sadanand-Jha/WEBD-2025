1a850b668376b4f9d289d74f3ce0d2f6ad7bd009
Sadanand Jha <coder.sadanand.dseu@gmail.com>
2025-08-14 12:21:15 +0530

    all set the env file

--- FireBase/youtube_app/youtube-app/app/firebase.tsx
+++ FireBase/youtube_app/youtube-app/app/firebase.tsx
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "app-435e9.firebaseapp.com",
-  projectId: "app-435e9",
-  storageBucket: "app-435e9.firebasestorage.app",
-  messagingSenderId: "60125748324",
-  appId: "1:60125748324:web:21b380de244688687d9371",
-  databaseURL: 'https://app-435e9-default-rtdb.firebaseio.com/'
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SERVER_ID,
+  appId: process.env.APP_ID,
+  databaseURL: process.env.DATABASE_URL
 };
 
 export const  app = initializeApp(firebaseConfig);
